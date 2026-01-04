f05dc30fa3853f83c950e6d43254892c34101951
Rizzo Rios <rizzo.rios@student.ipso.ch>
2026-01-04 14:30:26 +0100

    Manage Worker with Terraform (module worker)

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,11 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-locals {
-  route_host = "${var.worker_name}.${var.workers_subdomain}.workers.dev"
-}
+resource "cloudflare_worker_script" "worker" {
+  account_id = var.account_id
+  name       = var.worker_name
 
-# Route: <worker>.<account>.workers.dev/* -> Worker Script
-resource "cloudflare_worker_route" "workers_dev_route" {
-  zone_id     = "workers.dev"
-  pattern     = "${local.route_host}/*"
-  script_name = var.worker_name
+  content = file("${path.module}/../app/worker.js")
+
+  module = true
 }
